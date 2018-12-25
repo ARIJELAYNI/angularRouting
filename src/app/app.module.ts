d1aec20738cf8fd5b2d5ec76bd183b647b0a8cd0
Arij El Aini <arijelaini@arijs-air.speedport.ip>
2018-12-25 13:05:44 +0100

    first angular routing project steps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Route, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PersonDetailComponent } from './person-detail/person-detail.component';
+import {ContactListComponent} from './contact-list/contact-list.component';
+
+
+
+const routes: Route[] = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component : AboutComponent
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  },
+  {
+    path: 'people/persondetail',
+    component: PersonDetailComponent
+  },
+  {
+    path: 'contacts/list',
+    component: ContactListComponent
+  }
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotfoundComponent,
+    PersonDetailComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
